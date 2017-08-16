70fc636c2e882805b8a201511b3903fb9b1cce96
1306388383@qq.com <1306388383@qq.com>
2017-08-16 17:31:29 +0800

    修改工作台页面

--- js/common.js
+++ js/common.js
@@ -11,7 +11,7 @@
     //读取头部
     var data = JSON.parse(sessionStorage.getItem('USER'));
     if (data && data.role == 1) {
-        $('#nav').html('<li><a href="../../index.html">首页</a></li> <li class="menu_line"></li> <li><a href="../workbench/dashboard.html">我的工作台</a></li> <li class="menu_line"></li> <li><a href="../consul/consuladd.html">会诊/转诊</a></li> <li class="menu_line"></li> <li><a href="http://www.hancloudclinic.com:8084/FormParser">大数据平台</a></li> <li class="menu_line"></li> <li><a href="###">医联体论坛</a></li> <li class="menu_line"></li> <li><a href="###">便民服务</a></li><li class="menu_line"></li> <li><a href="###">在线咨询</a></li>');
+        $('#nav').html('<li><a href="../../index.html">首页</a></li> <li class="menu_line"></li> <li><a href="../workbench/todolist.html">我的工作台</a></li> <li class="menu_line"></li> <li><a href="../consul/consuladd.html">会诊/转诊</a></li> <li class="menu_line"></li> <li><a href="http://www.hancloudclinic.com:8084/FormParser">大数据平台</a></li> <li class="menu_line"></li> <li><a href="###">医联体论坛</a></li> <li class="menu_line"></li> <li><a href="###">便民服务</a></li><li class="menu_line"></li> <li><a href="###">在线咨询</a></li>');
         $('.head_tool').html('欢迎登录<span>' + data.uUserDTO.userName + '</span>' + '，' + '<span class="quit">' + '退出' + '</span>');
     } else if (data && data.role == 2) {
         $('#nav').html('<li><a href="../../index.html">首页</a></li> <li class="menu_line"></li> <li><a href="admin/index.html">运维管理</a></li> <li class="menu_line"></li> <li class="menu_line"></li> <li><a href="http://www.hancloudclinic.com:8084/FormParser">大数据平台</a></li> <li class="menu_line"></li> <li><a href="###">医联体论坛</a></li> <li class="menu_line"></li> <li><a href="###">便民服务</a></li><li class="menu_line"></li> <li><a href="###">在线咨询</a></li>');
