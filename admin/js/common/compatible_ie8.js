f0cba9fd47274835e197e0306655b2e0b8eb93ed
Ziv.zeng <Ziv.zeng@embracesource.com>
2017-08-22 09:26:14 +0800

    兼容代码

--- admin/js/common/compatible_ie8.js
+++ admin/js/common/compatible_ie8.js
@@ -3,31 +3,46 @@ if (!Array.isArray) {
         return Object.prototype.toString.call(arg) === '[object Array]';
     };
 }
-if (!Array.prototype.filter){
-    Array.prototype.filter = function(fun /*, thisArg */)
-    {
-        "use strict";
-
-        if (this === void 0 || this === null)
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function(callback) {
+        // 获取数组长度
+        var len = this.length;
+        if(typeof callback != "function") {
             throw new TypeError();
-
-        var t = Object(this);
-        var len = t.length >>> 0;
-        if (typeof fun !== "function")
-            throw new TypeError();
-
-        var res = [];
-        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-        for (var i = 0; i < len; i++)
-        {
-            if (i in t)
-            {
-                var val = t[i];
-                if (fun.call(thisArg, val, i, t))
-                    res.push(val);
+        }
+        // 创建新数组，用于承载经回调函数修改后的数组元素
+        var newArr = new Array();
+        // thisArg为callback 函数的执行上下文环境
+        var thisArg = arguments[1];
+        for(var i = 0; i < len; i++) {
+            if(i in this) {
+                if(callback.call(thisArg, this[i], i, this)) {
+                    newArr.push(val);
+                }
             }
         }
+        return newArr;
+    }
+}
+//indexOf()兼容ie8
+if (!Array.prototype.indexOf){
+    Array.prototype.indexOf = function(elt /*, from*/)
+    {
+        var len = this.length >>> 0;
 
-        return res;
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+            ? Math.ceil(from)
+            : Math.floor(from);
+        if (from < 0)
+            from += len;
+
+        for (; from < len; from++)
+        {
+            if (from in this &&
+                this[from] === elt)
+                return from;
+        }
+        return -1;
     };
 }
